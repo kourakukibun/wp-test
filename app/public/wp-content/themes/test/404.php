66ac384fb465e7341a539461141a77bce0faef14
tmori <kourakukibun@gmail.com>
2020-02-02 00:34:58 +0900

    [Add] 404

---
+++ app/public/wp-content/themes/test/404.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<body>
+
+<div class="l-wrapper" id="top">
+
+<!-- [ HEADER-AREA ] -->
+<?php include('template-parts/template-header.php'); ?>
+<!-- /[ HEADER-AREA ] -->
+
+<!-- [ CONTENT-AREA ] -->
+<div class="l-contents">
+<main class="l-main">
+
+<div class="l-main-inner">
+<div class="l-section">
+<h1 class="mod-heading2">該当のページはありません。</h1>
+</div><!-- /.l-section -->
+</div><!-- /.l-main-inner -->
+
+</main>
+</div><!-- /.l-contents -->
+<!-- /[ CONTENT-AREA ] -->
+
+<!-- [ FOOTER-AREA ] -->
+<?php include('template-parts/template-footer.php'); ?>
+<!-- /[ FOOTER-AREA ] -->
+
+</div><!-- /.l-wrapper -->
+
+<?php get_footer(); ?>
